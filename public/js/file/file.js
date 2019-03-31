5e4201ff3acbd8c4a229bb52063a4b536d783d34
CreeperSan <admin@creepersan.com>
2019-03-31 23:31:54 +0800

    File:Click to open directory

--- public/js/file/file.js
+++ public/js/file/file.js
@@ -5,11 +5,12 @@ const FILE_TYPE_DIRECTORY   = 2;
 var app = new Vue({
     el : "#file-app",
     data : {
-        path : "/home/creepersan",
+        path : "/Users/creepersan/TestDirectory",
         isShowLoading : false,
         files : []
     },
     methods : {
+        /* 一些格式化方法 */
         toFormatTime(time){
             let data = new Date(time);
             return data.getFullYear()+"-"+data.getMonth()+"-"+data.getDay()+" "+data.getHours()+":"+data.getMinutes()+":"+data.getSeconds();
@@ -93,12 +94,58 @@ var app = new Vue({
                 result.push(this.toFormatFileItem(tmpNetworkFileItem))
             });
             return result;
+        },
+        toFilePath(file){
+            const self = this;
+            if (typeof file === "string"){
+                return self.path+"/"+file;
+            } else {
+                return self.path+"/"+file.name;
+            }
+        },
+        /* 下面是点击事件 */
+        onFileSelectionClick(index){
+            const self = this;
+            if (index >= 0 && index < self.files.length){
+                // 点击了文件
+                const file = self.files[index];
+                self.getFolderContent(self.toFilePath(file));
+            } else {
+                log("【错误】当前没有第"+index+"个文件")
+            }
+        },
+        onJumpClick(){
+            const self = this;
+            let path = document.getElementById("pathInput").value;
+            console.log(path);
+            self.getFolderContent(path);
+        },
+        /* 一些快捷操作 */
+        getFolderContent(path=""){
+            const self = this;
+            self.showLoading(); // 显示加载中
+            let requestParam = {};
+            if (path !== "" ){
+                requestParam.path = path;
+            }
+            httpPost("/file/api/file-list", requestParam, function (response) {
+                self.files = self.toFormatFileItemList(response.data);
+                self.showFileList(); // 显示文件列表
+            })
+
+        },
+        /* 一些界面操作 */
+        showLoading(){
+            const self = this;
+            self.isShowLoading = true;
+        },
+        showFileList(){
+            const self = this;
+            self.isShowLoading = false;
         }
     },
     created : function () {
         const self = this;
-        httpPost("/file/api/file-list",{},function (response) {
-            self.files = self.toFormatFileItemList(response.data);
-        })
+        self.getFolderContent();
     }
 });
